5e3099502c7e2c47e8430583943ebbbfa6cca2ed
AnyueWang <moonwong.way@gmail.com>
2024-07-22 12:16:47 +0200

    add: buttons to add and delete experience in education

--- cv_app/src/components/EducationInfo.jsx
+++ cv_app/src/components/EducationInfo.jsx
@@ -1,10 +1,22 @@
+import helpers from "../utilities/helpers"
+import DeleteExperience from "./DeleteExperience"
+import AddExperience from "./AddExperience"
+
 const EducationInfo = ({ info, setInfo }) => {
+  const addNewExperience = () => {
+    setInfo(info.concat(helpers.newEducationInfo()))
+  }
+  const deleteExperience = (id) => {
+    setInfo(info.filter(each => each.id !== id))
+  }
   return (
     <form className="info-form">
       {
-        info.map(eachExp => {
+        info.map((eachExp, idx) => {
           return (
-            <div key={eachExp.id}>
+            <div key={eachExp.id} className="one-experience">
+              <DeleteExperience handleClick={() => deleteExperience(eachExp.id)} idx={idx} />
+
               <label htmlFor={'edu-degree-' + eachExp.id}>Degree:</label>
               <input
                 type="text"
@@ -20,7 +32,7 @@ const EducationInfo = ({ info, setInfo }) => {
           )
         })
       }
-
+      <AddExperience handleClick={addNewExperience} />
     </form>
   )
 }
