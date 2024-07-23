ae73e4d7df6753fa520a37a201cc29f723ef9827
AnyueWang <moonwong.way@gmail.com>
2024-07-23 11:01:30 +0200

    style: adjust cv's structure

--- cv_app/src/components/DisplayPage.jsx
+++ cv_app/src/components/DisplayPage.jsx
@@ -8,54 +8,89 @@ import githubIcon from '../assets/github.svg'
 const DisplayPage = ({ generalInfo, workInfo, educationInfo, projectInfo }) => {
   return (
     <div className="display-page">
-      <p>{generalInfo.firstName + ' ' + generalInfo.lastName}</p>
-      <p>{generalInfo.title}</p>
-      <p><img src={emailIcon} className='icon-display' />: {generalInfo.email}</p>
-      <p><img src={phoneIcon} className='icon-display' />: {generalInfo.telephone}</p>
-      <p><img src={addressIcon} className='icon-display' />: {generalInfo.address}</p>
-      <p><img src={websiteIcon} className='icon-display' />: {generalInfo.website}</p>
-      <p><img src={linkedinIcon} className='icon-display' />: {generalInfo.linkedin}</p>
-      <p><img src={githubIcon} className='icon-display' />: {generalInfo.github}</p>
-      <p>Summary: {generalInfo.summary}</p>
-      <p>Skills: {generalInfo.skills}</p>
-      <p>Languages: {generalInfo.languages}</p>
-      <div>
+      <div className='display-basic-info'>
+        <p className='display-name'>{generalInfo.firstName + ' ' + generalInfo.lastName}</p>
+        <p className='display-title'>{generalInfo.title}</p>
+        <p className='display-contain-icon'>
+          <img src={phoneIcon} className='icon-display' />: &nbsp;&nbsp;
+          {generalInfo.telephone}
+        </p>
+        <p className='display-contain-icon'>
+          <img src={addressIcon} className='icon-display' />: &nbsp;&nbsp;
+          {generalInfo.address}
+        </p>
+        <p className='display-contain-icon'>
+          <img src={emailIcon} className='icon-display' />:  &nbsp;&nbsp;
+          <a href={generalInfo.email}>{generalInfo.email}</a>
+        </p>
+        <p className='display-contain-icon'>
+          <img src={websiteIcon} className='icon-display' />: &nbsp;&nbsp;
+          <a href={generalInfo.website}>{generalInfo.website}</a>
+        </p>
+        <p className='display-contain-icon'>
+          <img src={linkedinIcon} className='icon-display' />: &nbsp;&nbsp;
+          <a href={generalInfo.linkedin}>{generalInfo.linkedin}</a>
+        </p>
+        <p className='display-contain-icon'>
+          <img src={githubIcon} className='icon-display' />: &nbsp;&nbsp;
+          <a href={generalInfo.github}>{generalInfo.github}</a>
+        </p>
+      </div>
+      <div className='display-section'>
+        <p className='display-subtitle'>Summary</p>
+        <p>{generalInfo.summary}</p>
+      </div>
+      <div className='display-section'>
+        <p className='display-subtitle'> Skills</p>
+        <p className='display-textarea'>{generalInfo.skills}</p>
+      </div>
+      <div className='display-section'>
+        <p className='display-subtitle'>Languages</p>
+        <p>{generalInfo.languages}</p>
+      </div>
+      <div className='display-section'>
+        <p className='display-subtitle'>Work Experience</p>
         {
           workInfo.map(each => {
             return (
-              <div key={each.id}>
-                <p>{each.title}</p>
-                <p>{each.company}</p>
-                <p>{each.location}</p>
-                <p>{`${each.from} - ${each.to}`}</p>
+              <div className='display-one-experience' key={each.id}>
+                <div className='display-experience-basic'>
+                  <p className='display-job-title'>{each.title}</p>
+                  <p className='display-period' hidden={!each.from || !each.to}>{`${each.from} - ${each.to}`}</p>
+                  <p>{each.company}, {each.location}</p>
+                </div>
                 <p>{each.description}</p>
               </div>
             )
           })
         }
       </div>
-      <div>
+      <div className='display-section'>
+        <p className='display-subtitle'>Education</p>
         {
           educationInfo.map(each => {
             return (
-              <div key={each.id}>
-                <p>{each.degree}</p>
-                <p>{each.major}</p>
-                <p>{each.institution}</p>
-                <p>{each.location}</p>
-                <p>{`${each.from} - ${each.to}`}</p>
+              <div className='display-one-experience' key={each.id}>
+                <div className='display-experience-basic'>
+                  <p><span className='display-edu-title'>{each.degree}</span>, {each.major}</p>
+                  <p className='display-period'>{`${each.from} - ${each.to}`}</p>
+                  <p>{each.institution}, {each.location}</p>
+                </div>
               </div>
             )
           })
         }
       </div>
-      <div>
+      <div className='display-section'>
+        <p className='display-subtitle'>Projects</p>
         {
           projectInfo.map(each => {
             return (
-              <div key={each.id}>
-                <p>{each.name}</p>
-                <p>{`${each.from} - ${each.to}`}</p>
+              <div className='display-one-experience' key={each.id}>
+                <div className='display-experience-basic'>
+                  <p className='display-pro-title'>{each.name}</p>
+                  <p className='display-period'>{`${each.from} - ${each.to}`}</p>
+                </div>
                 <p>{each.description}</p>
               </div>
             )
